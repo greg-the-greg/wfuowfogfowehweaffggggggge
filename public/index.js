10d79f7f3d68f81853fd65ae9c9c93a5f2a47f86
outrye <69704692+outrye@users.noreply.github.com>
2022-12-15 15:28:38 -0300

    mistak

--- public/index.js
+++ public/index.js
@@ -6,4 +6,4 @@ var options = {
    body: JSON.stringify({coords : "28,72,2,3",data: "wigga wogga", name : "SERVER"}) // body data type must match "Content-Type" header
 }
 console.log("here");
-fetch("https://wfuowfogfowehweaffggggggge.greg-the-greg.repl.co/discord",options).then(re=>console.log(re))
+fetch("/discord",options).then(re=>console.log(re))
